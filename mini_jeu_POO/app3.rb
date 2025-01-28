3b4130a61f417313f35d3281a05383666a6963fc
charlieSIGOGNEAU <charliesigogneau@gmail.com>
2025-01-28 19:21:51 +0100

    tous ormie quelque fonctionalite en plus

--- mini_jeu_POO/app3.rb
+++ mini_jeu_POO/app3.rb
@@ -4,5 +4,20 @@ Bundler.require
 require_relative 'lib/game'
 require_relative 'lib/player'
 
+
+puts "
+------------------------------------------------
+|Bienvenue sur 'ILS VEULENT TOUS MA POO' !      |
+|Le but du jeu est d'être le dernier survivant !|
+-------------------------------------------------
+"
 puts "Quel est ton nom ?"
-Game.new(gets.chomp)
+my_game = Game.new(gets.chomp)
+
+while my_game.si_still_ongoing? 
+  my_game.menu
+  my_game.menu_choice(gets.chomp)
+  my_game.enemies_attack 
+end
+my_game.game_over
+
--- mini_jeu_POO/lib/game.rb
+++ mini_jeu_POO/lib/game.rb
@@ -1,4 +1,4 @@
-class game
+class Game
   attr_accessor :human_player, :enemies
 
   def initialize (name)
@@ -6,7 +6,7 @@ class game
     4.times do |i|
       @enemies << Player.new("player#{i.to_s}")
     end
-    human_player = HumanPlayer.new(name)
+    @human_player = HumanPlayer.new(name)
   end
 
   def kill_player (player)
@@ -14,13 +14,47 @@ class game
   end
 
   def si_still_ongoing? 
-    @human_player.life_point > 0 && !(enemies.length > 0)
+    @human_player.life_point > 0 && @enemies.length > 0
   end
 
-  def
+  def show_payer
+    human_player.show_state
+    puts "Il reste #{@enemies.length} enemies"
+  end
 
+  def menu
+    puts "tu as #{@human_player.life_point} point de vie
+Quelle action veux-tu effectuer ?
+a - chercher une meilleure arme
+s - chercher à se soigner
+attaquer un joueur en vue :
+"
+    @enemies.each_with_index do |enemy, i|
+      print "#{i} - "
+      enemy.show_state
+    end
+  end
 
+  def menu_choice (choice)
+    human_player.search_weapon if choice == "a"
+    human_player.search_health_pack if choice == "s"
+    if choice.to_i >= 0 && choice.to_i <= @enemies.length && choice.to_i.to_s == choice
+      @human_player.attacks(@enemies[choice.to_i])
+      kill_player(@enemies[choice.to_i]) if @enemies[choice.to_i].life_point <= 0
+    end
+  end
 
+  def enemies_attack 
+    @enemies.each do |enemies|
+      enemies.attacks(@human_player)
+    end
+  end
 
+  def game_over
+    if human_player.life_point > 0
+      puts "BRAVO ! TU AS GAGNE !"
+    else
+      puts "Loser ! Tu as perdu !"
+    end
   end
 end
